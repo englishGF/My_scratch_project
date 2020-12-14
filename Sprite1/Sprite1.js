99f9f31b49c3bdac8fa5c4c014828e34e7cb44df
englishGF <53413219+englishGF@users.noreply.github.com>
2020-12-14 10:03:04 -0800

    changed

--- Sprite1/Sprite1.js
+++ Sprite1/Sprite1.js
@@ -17,14 +17,8 @@ export default class Sprite1 extends Sprite {
       new Costume("costume1", "./Sprite1/costumes/costume1.svg", {
         x: 48,
         y: 50
-      }),
-      new Costume("costume2", "./Sprite1/costumes/costume2.svg", {
-        x: 46,
-        y: 53
       })
     ];
-
-    this.sounds = [new Sound("Meow", "./Sprite1/sounds/Meow.wav")];
 
     this.triggers = [
       new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked)
